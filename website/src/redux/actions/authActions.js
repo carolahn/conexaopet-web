070d3f0898ed60f16004335a84b011d8f264656b
carolahn <ahncarolina@gmail.com>
2024-04-14 17:29:04 -0300

    chore: integração com o back 3

--- website/src/redux/actions/authActions.js
+++ website/src/redux/actions/authActions.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from '../../utils/axiosConfig';
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000/api';
+// axios.defaults.baseURL = 'http://127.0.0.1:5000/api';
 
 export const login = (username, password) => async (dispatch) => {
   try {
