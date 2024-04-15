4c1316a401662cf09e81959b03524a44e3b0ffa5
carolahn <ahncarolina@gmail.com>
2024-04-15 11:57:12 -0300

    chore: integração com o back 5

--- website/src/redux/store/index.js
+++ website/src/redux/store/index.js
@@ -8,6 +8,41 @@
 
 // export default store;
 
+//////////////////////////////////////////
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+// import { thunk } from 'redux-thunk';
+// import rootReducer from '../reducers';
+
+// const persistConfig = {
+//   key: 'root', // chave para armazenar os dados no armazenamento
+//   storage, // armazenamento a ser usado (localStorage, sessionStorage, etc.)
+//   // outros parâmetros opcionais aqui, como blacklist, whitelist, transformações, etc.
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const enhancer = composeWithDevTools(applyMiddleware(thunk));
+// const store = createStore(persistedReducer, enhancer);
+// const persistor = persistStore(store);
+
+// const logout = () => {
+//   return async (dispatch) => {
+//     try {
+//       // Limpa os dados persistidos
+//       await persistor.purge();
+      
+//       dispatch({ type: 'LOGOUT' });
+//     } catch (error) {
+//       console.error('Error logging out:', error);
+//     }
+//   };
+// };
+
+// export { store, persistor, logout };
+
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist';
@@ -23,7 +58,14 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const enhancer = composeWithDevTools(applyMiddleware(thunk));
+let enhancer = applyMiddleware(thunk);
+
+// Verifica se estamos em ambiente de desenvolvimento
+if (process.env.NODE_ENV === 'development') {
+  const { composeWithDevTools } = require('redux-devtools-extension');
+  enhancer = composeWithDevTools(enhancer);
+}
+
 const store = createStore(persistedReducer, enhancer);
 const persistor = persistStore(store);
 
